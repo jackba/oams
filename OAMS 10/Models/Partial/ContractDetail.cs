67cb90808e0a403f36883680721bd2a7dc3d7c42
nguyensaoky <nguyensaoky@e55b00a3-f482-2aa6-5422-8e550ae802a6>
2010-11-25 13:02:02 +0000

    Add ability to Edit Product and Category in ContractDetail

---
+++ OAMS 10/Models/Partial/ContractDetail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OAMS.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace OAMS.Models
+{
+
+    public partial class ContractDetail
+    {
+        public string CategoryFullName
+        {
+            get
+            {
+                CategoryRepository catRepository = new CategoryRepository();
+                return catRepository.GetFullname(Category1, Category2, Category3);
+            }
+        }
+
+        public string NewCategoryFullName
+        {
+            get;
+            set;
+        }
+
+        public int UpdateCategory(Guid? catID1, Guid? catID2, Guid? catID3)
+        {
+            CategoryID1 = catID1;
+            CategoryID2 = catID2;
+            CategoryID3 = catID3;
+
+            return 0;
+        }
+    }
+}
