31322e8d48a2b59ab8a84745b26399c28f2125e2
ghostnguyen@gmail.com <ghostnguyen@gmail.com@e55b00a3-f482-2aa6-5422-8e550ae802a6>
2010-12-02 15:50:56 +0000

    update data with new schema

--- trunk/OAMS 10/App_Data/SQLQuery1.sql
+++ trunk/OAMS 10/App_Data/SQLQuery1.sql
@@ -1,30 +1,25 @@
 
--- update SiteDetail set Product = 'blank' where ID = 1231
--- select * from SiteDetail where ProductID is null and Product is not null
-
--- select * from SiteDetail where (Product is not null and Product <> 'blank') and ClientID is not null
--- select * from Product where Name = 'blank'
-
-DECLARE SiteDetail_Cursor CURSOR FOR
-SELECT ID, ClientID, CategoryID1,CategoryID2,CategoryID3, Product
-FROM SiteDetail
-where (Product = 'blank') and ProductID is null
+DECLARE ContractDetail_Cursor CURSOR FOR
+SELECT ID, CategoryID1,CategoryID2,CategoryID3, Product
+FROM ContractDetail
+where  ProductID is null 
+ --where  ProductID is null and CategoryID1 is not null
+-- where (Product = 'blank') and ProductID is null
 -- where ProductID is null and Product is not null
--- where (Product = 'blank') and ClientID is not null
--- where (Product is not null and Product <> 'blank') and ClientID is not null
+-- where (Product = 'blank') or Product is null
+-- where (Product is not null and Product <> 'blank')
 -- group by ClientID, CategoryID1,CategoryID2,CategoryID3, Product
 
 
-OPEN SiteDetail_Cursor;
+OPEN ContractDetail_Cursor;
 declare @ID int, 
-@ClientID int, 
 @CatID1 uniqueidentifier, 
 @CatID2 uniqueidentifier, 
 @CatID3 uniqueidentifier,
 @Product nvarchar(max)
 
 
-FETCH NEXT FROM SiteDetail_Cursor INTO @ID,@ClientID,@CatID1,@CatID2,@CatID3,@Product;
+FETCH NEXT FROM ContractDetail_Cursor INTO @ID,@CatID1,@CatID2,@CatID3,@Product;
 
 WHILE @@FETCH_STATUS = 0
    BEGIN
@@ -39,23 +34,90 @@ WHILE @@FETCH_STATUS = 0
 			
 		if (@ProductID is null or @ProductID <= 0)
 		begin
-			insert into Product (Name,ClientID,CategoryID1,CategoryID2,CategoryID3) 
-				values (@Product,@ClientID,@CatID1,@CatID2,@CatID3)
+			insert into Product (Name,CategoryID1,CategoryID2,CategoryID3) 
+				values (@Product,@CatID1,@CatID2,@CatID3)
 			
 			set @ProductID = @@IDENTITY		
 		end
 			
 		if (@ProductID > 0)
 		begin
-			update SiteDetail set ProductID = @ProductID where ID = @ID
+			update ContractDetail set ProductID = @ProductID where ID = @ID
 		end
 		
-FETCH NEXT FROM SiteDetail_Cursor INTO @ID,@ClientID,@CatID1,@CatID2,@CatID3,@Product;
+FETCH NEXT FROM ContractDetail_Cursor INTO @ID,@CatID1,@CatID2,@CatID3,@Product;
   
    END;
-CLOSE SiteDetail_Cursor;
-DEALLOCATE SiteDetail_Cursor;
+CLOSE ContractDetail_Cursor;
+DEALLOCATE ContractDetail_Cursor;
 GO
+
+
+
+
+
+
+
+
+
+
+---- update SiteDetail set Product = 'blank' where ID = 1231
+---- select * from SiteDetail where ProductID is null and Product is not null
+
+---- select * from SiteDetail where (Product is not null and Product <> 'blank') and ClientID is not null
+---- select * from Product where Name = 'blank'
+
+--DECLARE SiteDetail_Cursor CURSOR FOR
+--SELECT ID, ClientID, CategoryID1,CategoryID2,CategoryID3, Product
+--FROM SiteDetail
+--where (Product = 'blank') and ProductID is null
+---- where ProductID is null and Product is not null
+---- where (Product = 'blank') and ClientID is not null
+---- where (Product is not null and Product <> 'blank') and ClientID is not null
+---- group by ClientID, CategoryID1,CategoryID2,CategoryID3, Product
+
+
+--OPEN SiteDetail_Cursor;
+--declare @ID int, 
+--@ClientID int, 
+--@CatID1 uniqueidentifier, 
+--@CatID2 uniqueidentifier, 
+--@CatID3 uniqueidentifier,
+--@Product nvarchar(max)
+
+
+--FETCH NEXT FROM SiteDetail_Cursor INTO @ID,@ClientID,@CatID1,@CatID2,@CatID3,@Product;
+
+--WHILE @@FETCH_STATUS = 0
+--   BEGIN
+--		declare @ProductID int
+		
+--		set @ProductID = (select top 1 ID from Product where 
+--			Name = @Product 
+--			and ((@CatID1 is null and CategoryID1 is null) or CategoryID1 = @CatID1)
+--			and ((@CatID2 is null and CategoryID2 is null) or CategoryID2 = @CatID2)
+--			and ((@CatID3 is null and CategoryID3 is null) or CategoryID3 = @CatID3)
+--			)
+			
+--		if (@ProductID is null or @ProductID <= 0)
+--		begin
+--			insert into Product (Name,ClientID,CategoryID1,CategoryID2,CategoryID3) 
+--				values (@Product,@ClientID,@CatID1,@CatID2,@CatID3)
+			
+--			set @ProductID = @@IDENTITY		
+--		end
+			
+--		if (@ProductID > 0)
+--		begin
+--			update SiteDetail set ProductID = @ProductID where ID = @ID
+--		end
+		
+--FETCH NEXT FROM SiteDetail_Cursor INTO @ID,@ClientID,@CatID1,@CatID2,@CatID3,@Product;
+  
+--   END;
+--CLOSE SiteDetail_Cursor;
+--DEALLOCATE SiteDetail_Cursor;
+--GO
 
 
 
